58ca27802409e4e02d108fc00f2f6e2e98e458b6
ProEngineerStroganov <Pro-engineer.Stroganov@yandex.ru>
2024-08-15 17:44:23 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,7 +1,7 @@
 def solution(request):
     answer = []
-    for i in range(1, request + 1):
-        for j in range(i + 1, request + 1):
+    for i in range(1, request):
+        for j in range(i + 1, request):
             if request % (i + j) == 0:
                 answer.append(str(i))
                 answer.append(str(j))
