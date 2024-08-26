8171ca3d678046f93fac1c72ee360c0263b7c7bc
ProEngineerStroganov <Pro-engineer.Stroganov@yandex.ru>
2024-08-26 20:21:12 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,24 @@
+'''Реализуйте класс House, объекты которого будут создаваться следующим образом:
+House('ЖК Эльбрус', 30)
+Объект этого класса должен обладать следующими атрибутами:
+self.name - имя, self.number_of_floors - кол-во этажей.'''
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    '''Также должен обладать методом go_to(new_floor), где new_floor - номер этажа(int), на который нужно приехать.
+Метод go_to выводит на экран(в консоль) значения от 1 до new_floor(включительно).
+Если же new_floor больше чем self.number_of_floors или меньше 1, то вывести строку "Такого этажа не существует".'''
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            print(new_floor)
+        else:
+            print("Такого этажа не существует")
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
